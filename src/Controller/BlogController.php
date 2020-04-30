8ef6953619244d804ff0d960e3643e1e92cc9fff
Florian Salducci <you@example.com>
2020-04-30 02:48:44 +0200

    symfo-blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Entity\Articls;
+use App\Repository\ArticlsRepository;
+use App\Form\ArticlsType;
+
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticlsRepository $repo,ObjectManager $manager)
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles'=> $articles
         ]);
     }
 
@@ -26,12 +40,66 @@ class BlogController extends AbstractController
         return $this->render('blog/home.html.twig');
     }
 
+
+
     /**
-     * @Route("/blog/article/12", name="blog_show")
+    * @Route("/blog/new", name="blog_create")
+     * * @Route("/blog/{id}/edit", name="blog_edit")
+    */
+
+    //Création et de modification du formulaire + Create twig
+    public function form(Articls $article = null,Request $request, ObjectManager $manager)
+    {
+        if(!$article){
+            $article = new Articls();
+        }
+
+        //Générer le form de maniere classique
+        //$form = $this->createFormBuilder($article)
+              //  ->add('title',)
+              //  ->add('content', )
+              //  ->add('image')
+              //  ->getForm();
+
+        //Permet de récuperer le form crée directement dans la console (php bin/console make:form)
+       $form = $this->createForm(ArticlsType::class, $article);
+
+        //Soumission du formulaire
+        $form->handleRequest($request);
+        //Permet de modifier le formulaire si il est deja crée
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId())
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' =>$article->getId()]);
+        }
+        dump($article);
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        return $this -> render('blog/create.html.twig' , 
+        [
+          'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
      */
 
-    public function show()
+
+    public function show(Articls $articls)
     {
-        return $this->render('blog/show.html.twig');
+        return $this->render(
+            'blog/show.html.twig',
+            [
+            'articls'=> $articls,]
+        
+        );
     }
 }
+   
---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Articls;
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for($i = 1; $i <=10; $i++){
+            $article = New Articls();
+            $article->setTitle("Titre de l'article n°$i")
+                    ->setContent("<p>Contenue de l'article n°$i</p>")
+                    ->setImage("http://placehold.it/350x150")
+                    ->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
