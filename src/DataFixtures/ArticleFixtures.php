0d750869da3f41abd75d7c9b87709fd6989f543a
Florian Salducci <you@example.com>
2020-05-01 20:53:23 +0200

    symfo-blog category comment

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,19 +6,59 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Articls;
+use App\Entity\Category;
+use App\Entity\Comment;
+
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <=10; $i++){
-            $article = New Articls();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenue de l'article n°$i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($article);
+        //Crée 3 catégories Faker
+        for($i = 1; $i <= 3; $i++){
+            $category = New Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            for($j = 1; $j <=mt_rand(4, 6); $j++){
+                $article = New Articls();
+
+                $content ='<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                for($k = 1; $k <= mt_rand(4,10); $k++) {
+
+                    $comment = New Comment();
+
+                    $content ='<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+
+                    $days =(New \DateTime())->diff($article->getCreatedAt
+                    ())->days;
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween('-' . $days . 'days'))
+                            ->setArtilcs($article);
+
+                    $manager->persist($comment);
+                }
+            }
+
+            //Crée entre 4 et 6 articles
         }
+
+
         $manager->flush();
     }
 }
---
+++ src/Entity/Category.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\articls", mappedBy="category")
+     */
+    private $Articles;
+
+    public function __construct()
+    {
+        $this->Articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|articls[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->Articles;
+    }
+
+    public function addArticle(articls $article): self
+    {
+        if (!$this->Articles->contains($article)) {
+            $this->Articles[] = $article;
+            $article->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(articls $article): self
+    {
+        if ($this->Articles->contains($article)) {
+            $this->Articles->removeElement($article);
+            // set the owning side to null (unless already changed)
+            if ($article->getCategory() === $this) {
+                $article->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
